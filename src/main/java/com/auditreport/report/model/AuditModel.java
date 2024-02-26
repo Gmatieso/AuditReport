de599e1d4af7a40b63e9876c2408a8f713015a7b
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2024-02-26 13:45:59 +0300

    Creating model in Audit and User done

--- src/main/java/com/auditreport/report/model/AuditModel.java
+++ src/main/java/com/auditreport/report/model/AuditModel.java
@@ -1,4 +1,35 @@
 package com.auditreport.report.model;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Audit")
 public class AuditModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "userId")
+    private  Long userId;
+
+    @Column(name = "userAction")
+    private String userAction;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "tableName")
+    private String tableName;
+
+    @Column(name = "recordId")
+    private Long recordId;
+
+
+    @Column(name = "datetime")
+    private Date datetime;
+
 }
--- src/main/java/com/auditreport/report/model/UserModel.java
+++ src/main/java/com/auditreport/report/model/UserModel.java
@@ -1,4 +1,17 @@
 package com.auditreport.report.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "userName")
+    private String userName;
 }
